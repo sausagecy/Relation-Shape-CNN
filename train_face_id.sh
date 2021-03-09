d618cef5d7c172d0344f6515828fea4bc31cf94a
Seven Kong <jiangchangyuan999@gmail.com>
2021-03-09 11:05:33 +0800

    Delete train_face_id.sh
    will upload again in the future

--- train_face_id.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-mkdir -p log
-now=$(date +"%Y%m%d_%H%M%S")
-log_name="Face_BU3DFE_ID_LOG_"$now""
-export CUDA_VISIBLE_DEVICES=1
-python -u train_face_id.py \
---config cfgs/config_ssn_face_id.yaml \
-2>&1|tee log/$log_name.log 
